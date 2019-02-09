c4559fb865dd1ddcea6dac674ca6c63fa943377a
Thiago Valverde <valverde.thiago@gmail.com>
2019-02-09 17:52:52 -0200

    Adicionando exceptions a camada de negócio

---
+++ src/main/java/br/com/sicred/votacao_pauta/exception/VotacaoAindaAbertaException.java
@@ -0,0 +1,4 @@
+package br.com.sicred.votacao_pauta.exception;
+
+public class VotacaoAindaAbertaException extends RuntimeException {
+}
---
+++ src/main/java/br/com/sicred/votacao_pauta/exception/VotacaoEncerradaException.java
@@ -0,0 +1,4 @@
+package br.com.sicred.votacao_pauta.exception;
+
+public class VotacaoEncerradaException extends RuntimeException {
+}
