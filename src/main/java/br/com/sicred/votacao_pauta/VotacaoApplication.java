8ccf125bfd07641a785eeaf2d94d24751d6bc6a5
Thiago Valverde <valverde.thiago@gmail.com>
2019-02-09 11:14:47 -0200

    Adicionada classe main para execução da aplicação

---
+++ src/main/java/br/com/sicred/votacao_pauta/VotacaoApplication.java
@@ -0,0 +1,16 @@
+package br.com.sicred.votacao_pauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author valverde.thiago
+ */
+@SpringBootApplication
+public class VotacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotacaoApplication.class, args);
+    }
+
+}
