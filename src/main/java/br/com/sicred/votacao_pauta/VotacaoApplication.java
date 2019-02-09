49c4f2e8ab9944948a48fde39e2b093e66a21ba6
Thiago Valverde <valverde.thiago@gmail.com>
2019-02-09 17:52:23 -0200

    Removendo APIs REST geradas através dos repositórios

--- src/main/java/br/com/sicred/votacao_pauta/VotacaoApplication.java
+++ src/main/java/br/com/sicred/votacao_pauta/VotacaoApplication.java
@@ -2,11 +2,12 @@ package br.com.sicred.votacao_pauta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
 /**
  * @author valverde.thiago
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class VotacaoApplication {
 
     public static void main(String[] args) {
