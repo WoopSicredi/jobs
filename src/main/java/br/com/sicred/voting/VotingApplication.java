f6b54c2024a63caa1ef8d2e24b61a205b41f95f5
Thiago Valverde <valverde.thiago@gmail.com>
2019-02-10 11:32:25 -0200

    Adicionado swagger para chamada dos endpoints e documentação da API

--- src/main/java/br/com/sicred/voting/VotingApplication.java
+++ src/main/java/br/com/sicred/voting/VotingApplication.java
@@ -2,16 +2,47 @@ package br.com.sicred.voting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
 
 /**
  * @author valverde.thiago
  */
-@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+@SpringBootApplication
+@EnableSwagger2
 public class VotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VotingApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.sicred.voting.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData())
+                .directModelSubstitute(LocalDate.class, String.class);
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Voting REST API ")
+                .description("\"API REST para gerenciamento de votações\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
     }
 
 }
