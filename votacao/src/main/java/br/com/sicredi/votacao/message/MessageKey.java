4c77833752a25bcff9ac12dc10a0e817f989543f
Alex Radavelli <alexradavelli@gmail.com>
2019-04-07 17:25:43 -0300

    Implementação de regras de validação ao salvar uma Sessão.

--- votacao/src/main/java/br/com/sicredi/votacao/message/MessageKey.java
+++ votacao/src/main/java/br/com/sicredi/votacao/message/MessageKey.java
@@ -8,10 +8,10 @@ public enum MessageKey {
 	ASSOCIADO_NAO_ENCONTRADO("erro.associadoNaoEncontradao"),
 	PAUTA_NAO_ENCONTRADA("erro.pautaNaoEncontrada"),
 	PARAMETROS_INVALIDOS("erro.parametrosInvalidos"),
-	PAUTA_INVALIDA("erro.pautaInvalida"),
-	SESSAO_INVALIDA("erro.sessaoInvalida"),
+	SESSAO_PAUTA_INEXISTENTE("erro.erro.sessaoComPautaInexistente"),
+	VOTO_SESSAO_INEXISTENTE("erro.votoComSessaoInexistente"),
+	VOTO_ASSOCIADO_INEXISTENTE("erro.votoComAssociadoInexistente"),
 	SESSAO_ENCERRADA("erro.sessaoEncerrada"), 
-	ASSOCIADO_INVALIDO("erro.AssociadoInvalido"),
 	ASSOCIADO_JA_VOTOU("erro.associadoJaVotou");
 	
 	private final String key;
--- votacao/src/main/java/br/com/sicredi/votacao/service/impl/SessaoServiceImpl.java
+++ votacao/src/main/java/br/com/sicredi/votacao/service/impl/SessaoServiceImpl.java
@@ -3,21 +3,41 @@ package br.com.sicredi.votacao.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.sicredi.votacao.exception.BusinessException;
+import br.com.sicredi.votacao.message.MessageKey;
 import br.com.sicredi.votacao.model.Sessao;
 import br.com.sicredi.votacao.repository.SessaoRepository;
+import br.com.sicredi.votacao.service.PautaService;
 import br.com.sicredi.votacao.service.SessaoService;
 
 @Service
 public class SessaoServiceImpl extends BaseServiceImpl<Sessao, Long> implements SessaoService {
+	
+	private final PautaService pautaService;
 
 	@Autowired
-	public SessaoServiceImpl(SessaoRepository repository) {
+	public SessaoServiceImpl(SessaoRepository repository, 
+			PautaService pautaService) {
 		super(repository);
+		this.pautaService = pautaService;
 	}
 	
 	@Override
 	protected SessaoRepository getRepository() {
 		return (SessaoRepository) super.getRepository();
+	}
+	
+	@Override
+	public Sessao save(Sessao sessao) {
+		validate(sessao);
+		return super.save(sessao);
+	}
+
+	private void validate(Sessao sessao) {
+		if (!pautaService.existsById(sessao.getPauta().getId())) {
+			throw new BusinessException(MessageKey.SESSAO_PAUTA_INEXISTENTE, 
+					sessao.getPauta().getId());
+		}
 	}
 
 }
