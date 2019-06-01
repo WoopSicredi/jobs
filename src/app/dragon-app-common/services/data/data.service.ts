d7515c7daa33e1156558d2fbc54456ce3dd1a9af
Guilherme Maron <guimaron@gmail.com>
2019-06-01 05:57:56 -0300

    Criado o data.service em COMMON, que será usado pelos outros services

--- src/app/dragon-app-common/services/data/data.service.ts
+++ src/app/dragon-app-common/services/data/data.service.ts
@@ -1,7 +1,10 @@
-
+import { Http }       from '@angular/http'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs/Observable'
 
-
+import 'rxjs/add/observable/throw'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/map'
 
 
 
@@ -9,9 +12,56 @@ import { Injectable } from '@angular/core'
 export class DataService 
 {
 
-  constructor () 
+  protected _http : Http
+  protected _url  : string
+
+
+
+  constructor (url : string, http: Http) 
   { 
 
+    this._http  =   http
+    this._url   =   url
+
+  }
+
+
+
+  public getAll ()
+  {
+
+    return (
+
+      this.http
+      .get(this.url)
+      .map( (response) => response.json() )
+      .catch(this.onError)
+
+    )
+
+  }
+
+
+
+  protected onError (originalError : Response)
+  {
+
+    return (
+      Observable.throw(originalError)
+    )
+
+  }
+
+
+
+  protected get http ()
+  {
+    return (this._http)
+  }
+
+  public get url ()
+  {
+    return (this._url)
   }
 
 }
