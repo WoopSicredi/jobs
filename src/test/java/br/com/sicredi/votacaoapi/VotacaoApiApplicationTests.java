8e880fcad9a363d1227d69dd9b6bbdc41897c95a
Ruddy Paz <ruddy.persivo@gmail.com>
2019-11-25 00:44:11 -0300

    Commit inicial

---
+++ src/test/java/br/com/sicredi/votacaoapi/VotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.votacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
