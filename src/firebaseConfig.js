195c28f34a090b8d44acf2899c50adbd11dd0d27
Alessandra Henkes Nepomoceno <alessandra.henkes@cwi.com.br>
2020-04-22 19:37:11 -0300

    Adicionando estrutura inicial

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: 'AIzaSyDbJkbvUnJs45XdcMOafrltQA8kYf0Vs3U',
+  authDomain: 'dracarys-90797.firebaseapp.com',
+  databaseURL: 'https://dracarys-90797.firebaseio.com',
+  projectId: 'dracarys-90797',
+  storageBucket: 'dracarys-90797.appspot.com',
+  messagingSenderId: '632050807466',
+  appId: '1:632050807466:web:2bb8723101192cc874d6d3',
+};
