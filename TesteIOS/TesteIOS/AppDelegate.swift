0edbe06f180408694b161dc9c333797f219de867
Levy Cristian <jolevy.cristian@gmail.com>
2020-10-23 09:13:49 -0300

    Setting up view code

--- TesteIOS/TesteIOS/AppDelegate.swift
+++ TesteIOS/TesteIOS/AppDelegate.swift
@@ -12,9 +12,12 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
     
